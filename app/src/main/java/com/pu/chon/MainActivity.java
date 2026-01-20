2a34f6e47dcb2072cf5ccaf424163227688d1c69
chon <std.68122710103@ubru.ac.th>
2026-01-20 15:15:08 +0700

    first commit

---
+++ app/src/main/java/com/pu/chon/MainActivity.java
@@ -0,0 +1,55 @@
+package com.pu.chon;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        Fragment homeFragment = new HomeFragment();
+        Fragment ProfileFragment = new profileFragment();
+        Fragment SettingFragment = new settingFragment();
+
+        setCurrentFragment(homeFragment);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.home){
+                setCurrentFragment(homeFragment);
+                return true;
+            }
+            else if (id == R.id.profile){
+                setCurrentFragment(ProfileFragment);
+                return true;
+            }
+            else if (id == R.id.settings){
+                setCurrentFragment(SettingFragment);
+                return true;
+            }
+
+            return false;
+        });
+    }
+
+
+    private void setCurrentFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.flFragment, fragment)
+                .commit();
+    }
+}
